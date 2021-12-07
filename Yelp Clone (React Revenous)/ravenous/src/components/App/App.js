db42c50d11bfdb99477c768aafa20e9f4cab9eef
Martin Mallol <martinmallolcc@gmail.com>
2021-12-07 13:23:27 -0300

    Actualizo App js y css y termino primera etapa de yelp

--- Yelp Clone (React Revenous)/ravenous/src/components/App/App.js
+++ Yelp Clone (React Revenous)/ravenous/src/components/App/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Importo los componentes 'Business List' y 'Search Bar'
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+
+// Lo transformo en componente
+class App extends React.Component {
+  // Ahora App.js utiliza los componentes creados (los que importe arriba)
+  render(){
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
+      </div>
+    );
+  };
 }
 
 export default App;
--- Yelp Clone (React Revenous)/ravenous/src/components/SearchBar/SearchBar.js
+++ Yelp Clone (React Revenous)/ravenous/src/components/SearchBar/SearchBar.js
@@ -13,10 +13,32 @@ const sortByOptions = {
 class SearchBar extends React.Component {
     // Se hace render de los items de la lista por key (itera entre ellas)
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map
-            (sortByOption => {
-                let attr = sortByOptions[sortByOption];
-                return <li key={attr}>{sortByOption}</li>
-            });
-    }
+      return Object.keys(sortByOptions).map
+          (sortByOption => {
+              let attr = sortByOptions[sortByOption];
+              return <li key={attr}>{sortByOption}</li>
+          });
+    };
+    // Metodo render de toda la vida
+    render(){
+      return (
+          <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+              <ul>
+                {this.renderSortByOptions()}
+              </ul>
+            </div>
+            <div className="SearchBar-fields">
+              <input placeholder="Search Businesses" />
+              <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+              <a>Let's Go</a>
+            </div>
+          </div>
+      );        
+    };
 };
+
+// Exporto el componente
+export default SearchBar;

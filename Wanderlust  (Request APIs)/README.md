ca329e63b223c04785cf9916eaf2b85e33dab20f
Martin Mallol <martinmallolcc@gmail.com>
2022-02-04 17:54:45 -0300

    Ejercicio sobre API Requests, promises, async... await

---
+++ Wanderlust  (Request APIs)/README.md
@@ -0,0 +1,21 @@
+# Wanderlust
+
+## Que hice en este ejercicio
+
+Trabaje solamente sobre el archivo **main.js**. Genere APIs de dos plataformas, **Foursquare** y **OpenWeather**. Con la primera pude conseguir data sobre locaciones geograficas. Mientras que la segunda me permitio obtener informacion sobre el clima en determinadas zonas. 
+
+Con este ejercicio puse en practica conceptos nuevos como las **APIs**, el **Request** de estas, **async... await**, **promises**, **responses**, **fetch()**, **.then()**, convertir objetos a **JSON**, la dinamica **try & catch**, y mas.
+
+## Indicaciones de Codecademy
+
+Using fetch, async, and await, you’ll request information from the Foursquare API and OpenWeather API to create a travel website.
+
+Before you begin, you’ll need to register for developer accounts for both of the APIs above. They’re both free.
+
+For Foursquare, once you make an account, navigate to your Projects page, click Create New Project and give it a fitting name. Within your new project, select Generate API Key, which you’ll also need to name, then the Foursquare API will supply you with an API Key. You’ll need to save this key in main.js.
+
+Note: You will only be able to see the Foursquare API Key once. Make sure you copy and paste it somewhere before dismissing the prompt. Should you ever lose your API key, be sure to revoke access to that key and generate a new one. All of these tasks can be completed from the Project Settings page.
+
+For OpenWeather, follow the instructions for the How to start. When prompted, create your account, you can fill in other for the OpenWeather questions for their data collection. OpenWeather will give you an API Key, which you’ll also need to save in main.js.
+
+Note: In this project, you will be using API Keys to authenticate the requests you make to the Foursquare and OpenWeather APIs. An API Key is your personal credential to access API resources and should never be shared. This means if you screenshot or send your code to someone for any reason, you should censor or remove your API Keys before doing so!
---
+++ Wanderlust  (Request APIs)/public/main.js
@@ -0,0 +1,99 @@
+// Foursquare API Info
+// Mi API Key de Foursquare
+const foursquareKey = 'fsq3g3a64c/3il1tza/n7LVTMuJjh+K+exw1SnxfxRfQhDg='; 
+// URL de place search API endpoint
+const url = 'https://api.foursquare.com/v3/places/search?near=';
+// Le agrego una query al final de la URL con 'near' como parametro
+
+// OpenWeather Info
+//Mi API Key sacada de la pag de Open Weather
+const openWeatherKey = 'a68a7c848ec1049af6d87017cfd20130';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $placeDivs = [$("#place1"), $("#place2"), $("#place3"), $("#place4")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Objeto que contiene informacion adicional sobre la Request que se realizara a la api de Foursquare
+const options = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    Authorization: foursquareKey
+  }
+};
+
+// Add AJAX functions here:
+// Esta funcion va a ser 'asynchronous' y va adevolver una Promesa
+const getPlaces = async () => {
+  // La variable va a tener como valor lo que el usuario escriba
+  const city = $input.val();
+  // Contiene la url del endpoint de Foursquare, el valor que ingreso el usuario, y tiene como limite 10 lugares
+  const urlToFetch = `${url}&${city}&limit=10`;
+  // Realizo un try (si puedo hago esto) catch (sino tiro error)
+  try {
+    // Creo un fetch. Este envia el objeto 'options' al endpoint de la API
+    // Ademas, devuelve una promesa que luego del await, se convierte en una response, 
+    //conteniendo el status de la promesa con informacion que la API recogio
+    const response = await fetch(urlToFetch, options);
+    // Si me devolvio una respuesta...
+    if (response.ok) {
+      // Convierto la respuesta a un 'JSON object'
+      const jsonResponse = await response.json();
+      // Logueo la respuesta a la consola
+      console.log(jsonResponse);
+      // El jsonResponse object tiene dos propiedades: 'context' y 'results'
+      // Una tiene coordenadas geograficas y la otra una lista de 10 objetos
+      // cada uno representando un lugar distinto
+
+      // Guardo los lugares pertenecientes a jsonResponse.results
+      const places = jsonResponse.results;
+      // Logueo la variable a la consola
+      console.log(places);
+
+      // Devuelvo el array places
+      return places;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+};
+
+const getForecast = () => {
+  
+};
+
+
+// Render functions
+const renderPlaces = (places) => {
+  $placeDivs.forEach(($place, index) => {
+    // Add your code here:
+
+    const placeContent = '';
+    $place.append(placeContent);
+  });
+  $destination.append(`<h2>${places[0].location.locality}</h2>`);
+};
+
+const renderForecast = (forecast) => {
+  const weatherContent = '';
+  $weatherDiv.append(weatherContent);
+};
+
+const executeSearch = () => {
+  $placeDivs.forEach(place => place.empty());
+  $weatherDiv.empty();
+  $destination.empty();
+  $container.css("visibility", "visible");
+  getPlaces();
+  getForecast();
+  return false;
+}
+
+$submit.click(executeSearch);

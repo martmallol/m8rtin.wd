61b4ad8b9845dd344bf6141d3adad69c9e82f002
Martin Mallol <martinmallolcc@gmail.com>
2022-02-07 14:56:08 -0300

    Hago renderizado de respuestas json a HTML

--- Wanderlust  (Request APIs)/public/main.js
+++ Wanderlust  (Request APIs)/public/main.js
@@ -1,13 +1,13 @@
 // Foursquare API Info
 // Mi API Key de Foursquare
-const foursquareKey = 'blablablalbla'; 
+const foursquareKey = 'blablabla'; 
 // URL de place search API endpoint
 const url = 'https://api.foursquare.com/v3/places/search?near=';
 // Le agrego una query al final de la URL con 'near' como parametro
 
 // OpenWeather Info
 //Mi API Key sacada de la pag de Open Weather
-const openWeatherKey = 'blalbalbla';
+const openWeatherKey = 'blablablabla';
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Page Elements
@@ -62,27 +62,57 @@ const getPlaces = async () => {
   } catch (error) {
     console.log(error);
   }
-
 };
 
-const getForecast = () => {
-  
+// Esta funcion va a ser 'asynchronous' y va adevolver una Promesa
+const getForecast = async () => {
+  // Mi url con la direc de la pag, el parametro del valor del input del usuario, y la API Key de la pagina Weather
+  const urlToFetch = `${weatherUrl}?q=${input.val()}&APPID=${openWeatherKey}`;
+  // Realizo un try (si puedo hago esto) catch (sino tiro error)
+  try {
+    // Creo un fetch, devuelve una promesa que luego del await, se convierte en una response, 
+    // conteniendo el status de la promesa con informacion que la API recogio
+    const response = await fetch(urlToFetch);
+    // Si me devolvio una respuesta...
+    if (response.ok) {
+      // Convierto la respuesta a un 'JSON object'
+      const jsonResponse = await response.json();
+      // Logueo la respuesta a la consola
+      console.log(jsonResponse);
+      // Devuelvo la respuesta que buscaba
+      return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 
 // Render functions
-const renderPlaces = (places) => {
-  $placeDivs.forEach(($place, index) => {
-    // Add your code here:
 
-    const placeContent = '';
+// En places obtengo todos los lugares de la API
+const renderPlaces = (places) => {
+  // En $placeDivs renderizo la informacion devuelta por la Foursquare API
+  $placeDivs.forEach(($place, index) => {
+    // Place object (+ info: https://developer.foursquare.com/reference/response-fields)
+    const place = places[index];
+    const placeIcon = place.categories[0].icon
+    const placeImgSrc = `${placeIcon.prefix}bg_64${placeIcon.suffix}`;
+    // Construyo el HTML con la info del lugar que me devolvio la API
+    // Uso una funcion llamada 'createPlaceHTML'. Se encuentra en public/helpers.js
+    // Renderizo la info
+    const placeContent = createPlaceHTML(place.name, place.location, placeImgSrc);
     $place.append(placeContent);
   });
   $destination.append(`<h2>${places[0].location.locality}</h2>`);
 };
 
+// En forecast obtengo el clima de la API (el jsonResponse devuelto por getForecast()). Voy a renderizar la info a $weatherDiv.
 const renderForecast = (forecast) => {
-  const weatherContent = '';
+  // Construyo el HTML (transformo el json object a un html object)
+  // Uso una funcion llamada 'createWeatherHTML'. Se encuentra en public/helpers.js
+  // Renderizo la info
+  const weatherContent = createWeatherHTML(forecast);
   $weatherDiv.append(weatherContent);
 };
 
@@ -91,8 +121,10 @@ const executeSearch = () => {
   $weatherDiv.empty();
   $destination.empty();
   $container.css("visibility", "visible");
-  getPlaces();
-  getForecast();
+  // Obtengo los lugares de la API, y luego, los renderizo 
+  getPlaces().then(places => renderPlaces(places));
+  // Obtengo el clima de la API, y luego, lo renderizo
+  getForecast().then(forecast => renderForecast(forecast));
   return false;
 }
 
